11a27f38083dec3394fd947c61fc761edabde9d1
Daniel Bengtsson <daniel.s.bengtsson@gmail.com>
2018-10-26 10:26:35 +0200

    RoboFriends
    Robofriends

---
+++ precache-manifest.4484e28c98be695f2efa25bb3a4cb1dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bfacfb3e856c9ad21eb6",
+    "url": "/static/js/main.bfacfb3e.chunk.js"
+  },
+  {
+    "revision": "307bf6a6b2af5b0d919d",
+    "url": "/static/js/1.307bf6a6.chunk.js"
+  },
+  {
+    "revision": "bfacfb3e856c9ad21eb6",
+    "url": "/static/css/main.6f181087.chunk.css"
+  },
+  {
+    "revision": "307bf6a6b2af5b0d919d",
+    "url": "/static/css/1.a4c63cd4.chunk.css"
+  },
+  {
+    "revision": "ec4d10c3b3f02e6c4bbae8aa36b50611",
+    "url": "/index.html"
+  }
+];
